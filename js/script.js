ee292175070ce0a639d3d4ae558cbc31872ac7ce
khris22 <khrispunzalan@gmail.com>
2020-06-29 18:48:54 -0400

    added heading, styling & animation using typedjs

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ $( document ).ready(function() {
         play: 4000,
         pagination: false
     });
+
+    var typed = new Typed('.typed', {
+        strings: ["Software Engineer", "Full Stack Web Developer", "Adventurer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });    
+
 });
 
 // $(function() {
