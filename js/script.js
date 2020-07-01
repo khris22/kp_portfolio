d6744401340008f661847d6e0d95a1f71b9b5411
khris22 <khrispunzalan@gmail.com>
2020-06-30 20:18:13 -0400

    styling portfolio images and icons

--- js/script.js
+++ js/script.js
@@ -70,7 +70,14 @@ $( document ).ready(function() {
         }
     });
 
+    $("[data-fancybox]").fancyBox();
     
-    
-
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 });
