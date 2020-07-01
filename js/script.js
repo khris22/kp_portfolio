14ae445cd0096c54216d476437f15b707f31611f
khris22 <khrispunzalan@gmail.com>
2020-06-30 22:16:39 -0400

    added navigation bar & debugged some scripts

--- js/script.js
+++ js/script.js
@@ -70,7 +70,8 @@ $( document ).ready(function() {
         }
     });
 
-    $("[data-fancybox]").fancyBox();
+  
+    $("[data-fancybox]").fancybox();
     
     $(".items").isotope({
         filter: '*',
@@ -80,4 +81,51 @@ $( document ).ready(function() {
             queue: false
         }
     })
+
+
+    $("#filters a").click(function() {
+
+    $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
+
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+        var targetElement = $(this).attr("href")
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({
+            scrollTop: targetPosition - 50
+        }, "slow")
+    })
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation)
+
+    function stickyNavigation() {
+        var body = $("body");
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px")
+            body.addClass("fixedNav");
+        }
+        else {
+            body.removeClass("fixedNav")
+            body.css("padding-top", 0)
+        }
+    }
+
 });
